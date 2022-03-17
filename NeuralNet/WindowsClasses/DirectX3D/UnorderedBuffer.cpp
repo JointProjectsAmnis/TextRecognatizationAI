8f56da160688a9185fc216b1d5d477dcb67f4e36
KoTeuKaSeeker <emaillit8@gmail.com>
2022-03-17 16:36:33 +0400

    #Заоопешил всё для компьютерного шейдера

--- NeuralNet/WindowsClasses/DirectX3D/UnorderedBuffer.cpp
+++ NeuralNet/WindowsClasses/DirectX3D/UnorderedBuffer.cpp
@@ -1,14 +1,43 @@
 #include "UnorderedBuffer.h"
 
-UnorderedBuffer::UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, void* data)
+UnorderedBuffer::UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, D3D11_USAGE usage, UINT bindFlags, UINT CPUAccessFlags)
 {
 	this->graphics = graphics;
 
 	D3D11_BUFFER_DESC desc = {};
-	desc.BindFlags = D3D11_BIND_UNORDERED_ACCESS;
+	desc.Usage = usage;
+	desc.BindFlags = bindFlags;
 	desc.ByteWidth = sizeData;
 	desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
-	desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	desc.CPUAccessFlags = CPUAccessFlags;
+	desc.StructureByteStride = stride;
+
+	HRESULT hr = graphics->device->CreateBuffer(&desc, NULL, &buffer);
+	if (FAILED(hr)) throw; //Возможно ошибка в том, что stride не кратен 4
+
+	if (bindFlags)
+	{
+		D3D11_UNORDERED_ACCESS_VIEW_DESC descView = {};
+		descView.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		descView.Buffer.FirstElement = 0;
+		descView.Format = DXGI_FORMAT_UNKNOWN;
+		descView.Buffer.NumElements = sizeData / stride;
+
+		hr = graphics->device->CreateUnorderedAccessView(buffer, &descView, &unorderedView);
+		if (FAILED(hr)) throw;
+	}
+}
+
+UnorderedBuffer::UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, void* data, D3D11_USAGE usage, UINT bindFlags, UINT CPUAccessFlags)
+{
+	this->graphics = graphics;
+
+	D3D11_BUFFER_DESC desc = {};
+	desc.Usage = usage;
+	desc.BindFlags = bindFlags;
+	desc.ByteWidth = sizeData;
+	desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+	desc.CPUAccessFlags = CPUAccessFlags;
 	desc.StructureByteStride = stride;
 
 	D3D11_SUBRESOURCE_DATA sd = {};
@@ -16,7 +45,7 @@ UnorderedBuffer::UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride,
 
 	HRESULT hr = graphics->device->CreateBuffer(&desc, &sd, &buffer);
 	if (FAILED(hr)) throw; //Возможно ошибка в том, что stride не кратен 4
-	
+
 
 	D3D11_UNORDERED_ACCESS_VIEW_DESC descView = {};
 	descView.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
@@ -31,15 +60,28 @@ UnorderedBuffer::UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride,
 void UnorderedBuffer::Map(D3D11_MAP mapType)
 {
 	D3D11_MAPPED_SUBRESOURCE mappedSub = {};
+	/*
+	* Здесь ошибка может произойти из-за того, что mapType не совпадает с CPUAccessFlags (указывается при создании буффера)
+	* 
+	* -Если при создании буффера CPUAccessFlags = D3D11_CPU_ACCESS_WRITE, то mapType должен быть равен D3D11_MAP_WRITE
+	* 
+	* -Если при создании буффера CPUAccessFlags = D3D11_CPU_ACCESS_READ,  то mapType должен быть равен D3D11_MAP_READ
+	*/
 	HRESULT hr = graphics->context->Map(buffer, 0, mapType, 0, &mappedSub);
 	if (FAILED(hr)) throw;
 	mappedAddress = (char*)mappedSub.pData;
 }
 
-int UnorderedBuffer::CopyMem(void* data, size_t sizeData, size_t offset)
+int UnorderedBuffer::WriteMem(void* data, size_t sizeData, size_t offset)
 {
 	memcpy(mappedAddress + offset, data, sizeData);
 	return offset + sizeData;
+}
+
+int UnorderedBuffer::ReadMem(void* data, size_t sizeData, size_t offset)
+{
+	memcpy(data, mappedAddress + offset, sizeData);
+	return sizeData + offset;
 }
 
 void UnorderedBuffer::UnMap()
@@ -50,4 +92,9 @@ void UnorderedBuffer::UnMap()
 void UnorderedBuffer::Bind(UINT slot)
 {
 	graphics->context->CSSetUnorderedAccessViews(slot, 1, &unorderedView, NULL);
+}
+
+void UnorderedBuffer::CopyResource(ID3D11Buffer* buffer)
+{
+	graphics->context->CopyResource(this->buffer, buffer);
 }
--- NeuralNet/WindowsClasses/DirectX3D/UnorderedBuffer.h
+++ NeuralNet/WindowsClasses/DirectX3D/UnorderedBuffer.h
@@ -12,13 +12,19 @@ public:
 	Graphics* graphics;
 
 	//stride должен быть кратен 4
-	UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, void* data);
+	UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, D3D11_USAGE usage = D3D11_USAGE_DEFAULT, UINT bindFlags = D3D11_BIND_UNORDERED_ACCESS, UINT CPUAccessFlags = D3D11_CPU_ACCESS_WRITE);
 
-	void Map(D3D11_MAP mapType = D3D11_MAP_READ_WRITE);
-	int CopyMem(void* data, size_t sizeData, size_t offset);
+	//stride должен быть кратен 4
+	UnorderedBuffer(Graphics* graphics, UINT sizeData, UINT stride, void* data, D3D11_USAGE usage = D3D11_USAGE_DEFAULT, UINT bindFlags = D3D11_BIND_UNORDERED_ACCESS, UINT CPUAccessFlags = D3D11_CPU_ACCESS_WRITE);
+
+	void Map(D3D11_MAP mapType = D3D11_MAP_WRITE_DISCARD);
+	int WriteMem(void* data, size_t sizeData, size_t offset);
+	int ReadMem(void* data, size_t sizeData, size_t offset);
 	void UnMap();
 
 	void Bind(UINT slot);
+
+	void CopyResource(ID3D11Buffer* buffer);
 
 private:
 	char* mappedAddress;
